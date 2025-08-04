e5892787842bba7afe4af15e17bb071d6772677f
Salah Eddine EL MENYIY <76744964+SalahEddineELMENYIY@users.noreply.github.com>
2025-08-04 23:55:42 +0100

    create a layout
    create components (footer and header)
    
    add a layout to combine the components with the main part

--- FrontEnd/src/routers/index.jsx
+++ FrontEnd/src/routers/index.jsx
@@ -1,24 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Home from '../pages/home.jsx'
-import Services from '../pages/services.jsx'
-import AboutUs from '../pages/AboutUs.jsx'
-import NotFound from '../pages/notFound.jsx'
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home.jsx";
+import Services from "../pages/services.jsx";
+import AboutUs from "../pages/AboutUs.jsx";
+import NotFound from "../pages/notFound.jsx";
+import Layout from "../layouts/layout.jsx";
 
-export const router  = createBrowserRouter( [
-  {
-    path : '/',
-    element : <Home />,
-  },
-  {
-    path : '/services',
-    element : <Services />,
-  },
-  {
-    path : '/about-us',
-    element : <AboutUs />,
-  },
+export const router = createBrowserRouter([
     {
-    path : '*',
-    element : <NotFound />,
-  }
-])
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/services",
+                element: <Services />,
+            },
+            {
+                path: "/about-us",
+                element: <AboutUs />,
+            },
+            {
+                path: "*",
+                element: <NotFound />,
+            },
+        ],
+    },
+]);
